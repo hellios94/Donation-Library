ea86e17e843ca8cd0c447bf25983bafa8c85d641
Hellios94 <hellios94@students.softuni.bg>
2019-04-21 22:43:10 +0300

    Food Lover

---
+++ Food Lover/FoodLover/FoodLover.Data/FoodLoverDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodLover.Data
+{
+    public class FoodLoverDbContext : DbContext
+    {
+        public FoodLoverDbContext(DbContextOptions<FoodLoverDbContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
---
+++ Food Lover/FoodLover/FoodLover.Model/Salads.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodLover.Model
+{
+    class Salads
+    {
+    }
+}
